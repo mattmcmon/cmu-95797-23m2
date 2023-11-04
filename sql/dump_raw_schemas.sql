63a2d57cc1538863f915599f189d39d4234e2eef
Matt McMonagle <mmcmon1990@gmail.com>
2023-11-04 10:00:40 -0400

    Updates to ingest.sql and dump_raw_schemas.sql

--- sql/dump_raw_schemas.sql
+++ sql/dump_raw_schemas.sql
@@ -1,29 +1,10 @@
 .echo on
 
-SHOW bike_data;
-
-DESCRIBE bike_data;
-
-SHOW central_park_weather;
-
-DESCRIBE central_park_weather;
-
-SHOW fhv_bases;
-
-DESCRIBE fhv_bases;
-
-SHOW fhv_tripdata;
-
-DESCRIBE fhv_tripdata;
-
-SHOW fhvhv_tripdata;
-
-DESCRIBE fhvhv_tripdata;
-
-SHOW green_tripdata;
-
-DESCRIBE green_tripdata;
-
-SHOW yellow_tripdata;
-
-DESCRIBE yellow_tripdata;
+SHOW TABLES;
+DESCRIBE "bike_data";
+DESCRIBE "central_park_weather";
+DESCRIBE "fhv_bases";
+DESCRIBE "fhv_tripdata";
+DESCRIBE "fhvhv_tripdata";
+DESCRIBE "green_tripdata";
+DESCRIBE "yellow_tripdata";
--- sql/ingest.sql
+++ sql/ingest.sql
@@ -1,34 +1,34 @@
 -- Creates bike_data table from file
-create table bike_data as select * from
+create or replace table bike_data as select * from
 read_csv_auto('./data/citibike-tripdata*.csv.gz',
-union_by_name=True, filename=True, header=True);
+union_by_name=True, filename=True, all_varchar=1, header=True);
 
 -- Creates central_park_weather table from file
-create table central_park_weather as select * from
+create or replace table central_park_weather as select * from
 read_csv_auto('./data/central_park_weather*.csv',  
-union_by_name=True, filename=True, header=True);
+union_by_name=True, filename=True, all_varchar=1, header=True);
 
 -- Creates fhv_bases table from file
-create table fhv_bases as select * from
+create or replace table fhv_bases as select * from
 read_csv_auto('./data/fhv_bases.csv',  
-union_by_name=True, filename=True, header=True);
+union_by_name=True, filename=True, all_varchar=1, header=True);
 
 -- Creates fhv_tripdata table from file
-create table fhv_tripdata as select * from
+create or replace table fhv_tripdata as select * from
 read_parquet('./data/taxi/fhv_tripdata*.parquet',
 union_by_name=True, filename=True);
 
 -- Creates fhvhv_tripdata table from file
-create table fhvhv_tripdata as select * from
+create or replace table fhvhv_tripdata as select * from
 read_parquet('./data/taxi/fhvhv_tripdata*.parquet',
 union_by_name=True, filename=True);
 
 -- Creates green_tripdata table from file
-create table green_tripdata as select * from
+create or replace table green_tripdata as select * from
 read_parquet('./data/taxi/green_tripdata*.parquet',
 union_by_name=True, filename=True);
 
 -- Creates yellow_tripdata table from file
-create table yellow_tripdata as select * from
+create or replace table yellow_tripdata as select * from
 read_parquet('./data/taxi/yellow_tripdata*.parquet',
 union_by_name=True, filename=True);
